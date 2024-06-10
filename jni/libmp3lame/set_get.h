ac29a30f5d89a20456f0c42912c6b679a609abb0
Anatta <1@fxcdev.com>
2024-06-10 22:25:09 +0800

    <feat> Fix compile err

--- jni/libmp3lame/set_get.h
+++ jni/libmp3lame/set_get.h
@@ -21,21 +21,21 @@
 #ifndef __SET_GET_H__
 #define __SET_GET_H__
 
-#include <lame.h>
+#include "lame.h"
 
 #if defined(__cplusplus)
-extern  "C" {
+extern "C"
+{
 #endif
 
-/* select psychoacoustic model */
+    /* select psychoacoustic model */
 
-/* manage short blocks */
+    /* manage short blocks */
     int CDECL lame_set_short_threshold(lame_global_flags *, float, float);
     int CDECL lame_set_short_threshold_lrm(lame_global_flags *, float);
     float CDECL lame_get_short_threshold_lrm(const lame_global_flags *);
     int CDECL lame_set_short_threshold_s(lame_global_flags *, float);
     float CDECL lame_get_short_threshold_s(const lame_global_flags *);
-
 
     int CDECL lame_set_maskingadjust(lame_global_flags *, float);
     float CDECL lame_get_maskingadjust(const lame_global_flags *);
@@ -43,32 +43,29 @@ extern  "C" {
     int CDECL lame_set_maskingadjust_short(lame_global_flags *, float);
     float CDECL lame_get_maskingadjust_short(const lame_global_flags *);
 
-/* select ATH formula 4 shape */
+    /* select ATH formula 4 shape */
     int CDECL lame_set_ATHcurve(lame_global_flags *, float);
     float CDECL lame_get_ATHcurve(const lame_global_flags *);
 
     int CDECL lame_set_preset_notune(lame_global_flags *, int);
 
-/* substep shaping method */
+    /* substep shaping method */
     int CDECL lame_set_substep(lame_global_flags *, int);
     int CDECL lame_get_substep(const lame_global_flags *);
 
-/* scalefactors scale */
+    /* scalefactors scale */
     int CDECL lame_set_sfscale(lame_global_flags *, int);
     int CDECL lame_get_sfscale(const lame_global_flags *);
 
-/* subblock gain */
+    /* subblock gain */
     int CDECL lame_set_subblock_gain(lame_global_flags *, int);
     int CDECL lame_get_subblock_gain(const lame_global_flags *);
 
-
-
-/*presets*/
-    int     apply_preset(lame_global_flags *, int preset, int enforce);
+    /*presets*/
+    int apply_preset(lame_global_flags *, int preset, int enforce);
 
     void CDECL lame_set_tune(lame_t, float); /* FOR INTERNAL USE ONLY */
     void CDECL lame_set_msfix(lame_t gfp, double msfix);
-
 
 #if defined(__cplusplus)
 }
